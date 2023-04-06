dbae52b8abac7b26b80e5f6a21af35c312f92a05
LuxScintilla <catherina.ea@gmail.com>
2023-04-06 11:28:30 +0100

    Add validation styles

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// const submitButton = document.querySelector(".submit-button");
+// const form = document.querySelector(".form");
+
+// submitButton.addEventListener("click", function() {
+//     form.className="submitted";
+// });
