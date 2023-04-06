cc079d695dbfcc6f05d7b6147f26f78072f76d15
LuxScintilla <catherina.ea@gmail.com>
2023-04-06 20:34:47 +0100

    Add validation and responsive

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
-// const submitButton = document.querySelector(".submit-button");
-// const form = document.querySelector(".form");
+const form = document.querySelector(".form");
+const inputField = document.querySelector("input");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const submitButton = document.querySelector(".submit-button");
 
-// submitButton.addEventListener("click", function() {
-//     form.className="submitted";
-// });
+function checkPassword() {
+    if(password.value === confirmPassword.value) {
+        password.style.backgroundColor = "rgba(0, 255, 0, 0.1)"
+        confirmPassword.style.backgroundColor = "rgba(0, 255, 0, 0.1)"
+    } else {
+        password.style.backgroundColor = "rgba(255, 0, 0, 0.1)"
+        confirmPassword.style.backgroundColor = "rgba(255, 0, 0, 0.1)"
+    }
+}
